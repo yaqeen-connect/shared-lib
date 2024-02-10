2c86cc97d0f3554f4cab76a9db7213bb7f21a453
Kaym Kassai <kaymkassai@kayms-mbp.home>
2024-02-10 09:32:22 +0100

    Library update

--- types/enums.ts
+++ types/enums.ts
@@ -1,5 +1,22 @@
-export enum ROLE {
+export enum ACCOUNT_TYPE {
   ADMIN = "admin",
   MODERATOR = "moderator",
   USER = "user",
+}
+
+export enum GENDER {
+  MALE = "male",
+  FEMALE = "female",
+  OTHER = "other",
+}
+
+export enum ACCOUNT_STATUS {
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+  BANNED = "banned",
+}
+
+export enum SUBSCRIPTION_PLAN {
+  FREE = "free",
+  PREMIUM = "premium",
 }
