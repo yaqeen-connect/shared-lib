39b2e9fdb0d1bc4d161fbe575884854999da8787
Kaym Kassai <kaymkassai@Kayms-MacBook-Pro.local>
2024-07-03 01:21:27 +0200

    Library update

--- types/constants.ts
+++ types/constants.ts
@@ -1,1 +1,22 @@
-export const TEST = "test";
+export const AVATARS = [
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871893/thumbs-1710871579402_rkwu7x.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871893/thumbs-1710871576139_kvczzh.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871893/thumbs-1710871573577_hluhao.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871893/thumbs-1710871571481_ljqv8a.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871892/thumbs-1710871568370_uisbev.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871892/thumbs-1710871565502_od21t0.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871892/thumbs-1710871562777_fjcd9u.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871892/thumbs-1710871560078_jfmdjm.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871892/thumbs-1710871557319_wr3jcx.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871891/thumbs-1710871555356_cxcakw.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871891/thumbs-1710871553093_bgylky.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871891/thumbs-1710871550118_aimwzg.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871891/thumbs-1710871546752_jkchov.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871541227_ixxzzs.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871534358_lstveg.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871525171_yluzrt.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871518455_mu3uni.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871511484_fkomn7.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871497843_iakkje.png",
+  "https://res.cloudinary.com/dnx0bp55y/image/upload/v1710871890/thumbs-1710871514805_bhwitf.png",
+];
