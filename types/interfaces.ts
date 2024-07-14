caa741dcb4d4351758d5de53e3f805e7c1bbd263
Kaym Kassai <kaymkassai@Kayms-MacBook-Pro.local>
2024-07-14 14:44:59 +0200

    Library update

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -2,3 +2,24 @@ export interface IPROFILE_PICTURE {
   public_id: string;
   url: string;
 }
+
+interface IAyah {
+  number: number;
+  text: string;
+  numberInSurah: number;
+  juz: number;
+  manzil: number;
+  page: number;
+  ruku: number;
+  hizbQuarter: number;
+  sajda: boolean;
+}
+
+interface ISurah {
+  number: number;
+  name: string;
+  englishName: string;
+  englishNameTranslation: string;
+  revelationType: string;
+  ayahs: IAyah[];
+}
